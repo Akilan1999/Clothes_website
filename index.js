5d6ebef65bf3122a51e76efc51b21334076fdc08
Akilan Selvacoumar <Akilan@Selvacoumars-MacBook-Pro.local>
2018-10-15 10:15:16 +0400

    Login and registeration page

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ app.get('/', (req,res) => {
     res.render('pages/search_result');
   });
   app.get('/cart', (req,res) => {
-   
     res.render('pages/cart');
-    
   });
 
+  app.get('/register', (req,res) => {
+    res.render('pages/register'); 
+  });
+
+  app.get('/login', (req,res) => {
+    res.render('pages/login');
+  });
+  app.get('/brand', (req,res) => {
+    res.render('pages/brand');
+  });
+  app.get('/brand_detail', (req,res) => {
+    res.render('pages/brand_detail');
+  });
+
+  
   // PORT
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listensing ${port}`));
